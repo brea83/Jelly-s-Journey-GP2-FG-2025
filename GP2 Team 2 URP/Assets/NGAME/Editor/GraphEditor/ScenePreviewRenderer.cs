95dc7a49d990f0ccbf87e9ff9dccf273022cbf4c
Briana <brea83@gmail.com>
2026-04-10 15:32:03 +0200

    start of painting indicators into the scene preview
    starting with spawners, and a refrence of the bottom left and top right pixels. currently spawners are 1 magenta pixel. will update to make them more visible and color coded now that I've figured out translating position between spaces.

--- GP2 Team 2 URP/Assets/NGAME/Editor/GraphEditor/ScenePreviewRenderer.cs
+++ GP2 Team 2 URP/Assets/NGAME/Editor/GraphEditor/ScenePreviewRenderer.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
 
 namespace NGAME.Editor
 {
@@ -11,7 +15,15 @@ namespace NGAME.Editor
 
             Camera camera = InitPreviewCamera(aScene, bounds);
 
-            return DrawScene(bounds, camera, renderTextureHeight);
+            Texture2D result = DrawScene(bounds, camera, renderTextureHeight);
+            Color[] pixels = result.GetPixels();
+            MarkSpawners(sceneData, result, new Vector2Int(result.width, result.height), camera);
+            result.Apply(false);
+            return result;
+        }
+        void DelayWritingSpawnerData()
+        {
+
         }
 
         private static Camera InitPreviewCamera(Scene aScene, SceneBounds bounds)
@@ -32,6 +44,8 @@ namespace NGAME.Editor
 
             camera.orthographicSize = bounds.Height();
 
+            
+
             return camera;
         }
 
@@ -41,11 +55,74 @@ namespace NGAME.Editor
             RenderTexture renderTexture = new RenderTexture(Mathf.RoundToInt(aspectRatio * renderTextureHeight), renderTextureHeight, 16);
             camera.targetTexture = renderTexture;
             camera.Render();
+
             Texture2D textureResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
-            textureResult.Apply(false);
-            Graphics.CopyTexture(renderTexture, textureResult);
+
+            //textureResult.Apply(false);
+            //Graphics.CopyTexture(renderTexture, textureResult);
+
+            RenderTexture oldActive = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            textureResult.ReadPixels(new Rect(0,0, renderTexture.width, renderTexture.height), 0, 0);
+
+            RenderTexture.active = oldActive;
 
             return textureResult;
+        }
+
+        private static void MarkSpawners(SceneData sceneData, Texture2D texture, Vector2Int actualTextureSize, Camera camera)
+        {
+            SceneBounds bounds = sceneData.Bounds;
+
+            Color[] pixels = texture.GetPixels();
+            //texture.
+            //List<Vector2Int> newSpawnPositions = new();
+            Vector3 worldMin3D = camera.ScreenToWorldPoint(new Vector3(0, 0, 10));
+            Vector3 worldMax3D = camera.ScreenToWorldPoint(new Vector3(actualTextureSize.x - 1, actualTextureSize.y - 1, 10));
+
+            Vector2 worldMin = new Vector2(worldMin3D.x, worldMin3D.z);
+            Vector2 worldMax = new Vector2(worldMax3D.x, worldMax3D.z);
+
+            Vector2 worldMaxRemaped = RemapVector2(worldMax, worldMin/*bounds.MinPoint*/, worldMax/*bounds.MaxPoint*/, new Vector2(0.0f, 0.0f), new Vector2(actualTextureSize.x - 1, actualTextureSize.y - 1));
+            int indexMax = Mathf.FloorToInt(worldMaxRemaped.y) * (actualTextureSize.x) + Mathf.FloorToInt(worldMaxRemaped.x);
+            if (indexMax < pixels.Length || indexMax > 0)
+            {
+                pixels[indexMax] = Color.red;
+            }
+            pixels[0] = Color.blue;
+
+            foreach (SpawnerData spawner in sceneData.SpawnPoints)
+            {
+                Vector2 position = new Vector2(spawner.Position.x, spawner.Position.z);
+
+                position = RemapVector2(position, worldMin/*bounds.MinPoint*/, worldMax/*bounds.MaxPoint*/, new Vector2(0.0f, 0.0f), new Vector2(actualTextureSize.x - 1, actualTextureSize.y -1));
+
+                int x = Mathf.FloorToInt(position.x);
+                int y = Mathf.FloorToInt(position.y);
+                int index = y * (actualTextureSize.x) + x;
+                if(index >= pixels.Length || index < 0)
+                {
+                    Debug.LogWarning("INDEX MATH WRONG");
+                    return;
+                }
+                pixels[index] = Color.magenta;
+            }
+            texture.SetPixels(pixels);
+            //texture.Apply(false);
+        }
+
+        public static float Remap(float input, float inputRangeMin, float inputRangeMax, float outputRangeMin, float outputRangeMax)
+        {
+            float t = Mathf.InverseLerp(inputRangeMin, inputRangeMax, input);
+            float output = Mathf.Lerp(outputRangeMin, outputRangeMax, t);
+            return output;
+        }
+
+        public static Vector2 RemapVector2(Vector2 input, Vector2 inputRangeMin, Vector2 inputRangeMax, Vector2 outputRangeMin, Vector2 outputRangeMax)
+        {
+            float newX = Remap(input.x, inputRangeMin.x, inputRangeMax.x, outputRangeMin.x, outputRangeMax.x);
+            float newY = Remap(input.y, inputRangeMin.y, inputRangeMax.y, outputRangeMin.y, outputRangeMax.y);
+            return new Vector2(newX, newY);
         }
     }
 }
