f4b7726193f2b8ed43c423c84e44f7cb088bc37f
Briana <brea83@gmail.com>
2026-04-25 16:09:59 +0200

    finally added a way to fastforward and skip through cutscenes
    scroll wheel now speeds up and slows down time in cutscenes. time is reset to normal on end of a cutscene.

--- GP2 Team 2 URP/Assets/Scripts/Ui/CutScenes/CutScene.cs
+++ GP2 Team 2 URP/Assets/Scripts/Ui/CutScenes/CutScene.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CutScene : MonoBehaviour
 {
@@ -10,6 +12,19 @@ public class CutScene : MonoBehaviour
     [Header("Debug Stuff")]
     public bool PrintDebugLogs = false;
 
+    bool m_CutSceneStarted = false;
+    InputAction m_UiScrollWheel;
+    float m_TimeMultiplier = 1.0f;
+    float m_TimeMultIncrement = 0.5f;
+
+    private void OnEnable()
+    {
+        if(m_UiScrollWheel != null)
+        {
+            m_UiScrollWheel.performed -= OnScrollWheelPerformed;
+            m_UiScrollWheel.performed += OnScrollWheelPerformed;
+        }
+    }
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -17,7 +32,44 @@ public class CutScene : MonoBehaviour
         //_menuController = GetComponentInParent<UiMenuController>();
         _cutsceneCanvas.enabled = false;
         if (PrintDebugLogs) Debug.Log($"--- CutScene.Start() found animator ({_animator}) and canvas ({_cutsceneCanvas}) ---");
+
+        GameObject playerObject = GameManager.Instance.player;
+        if (playerObject == null)
+            return;
+
+        PlayerInput input = playerObject.GetComponent<PlayerInput>();
+        if (input == null)
+            return;
+
+        m_UiScrollWheel = input.actions.FindAction("ScrollWheel");
+        if (m_UiScrollWheel == null)
+            return;
+
+        m_UiScrollWheel.performed += OnScrollWheelPerformed;
     }
+
+    private void OnDisable()
+    {
+        if( m_UiScrollWheel != null )
+            m_UiScrollWheel.performed -= OnScrollWheelPerformed;
+    }
+
+    private void OnScrollWheelPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 inputValue = context.ReadValue<Vector2>();
+
+        if (!m_CutSceneStarted || inputValue.y == 0)
+            return;
+
+        if (inputValue.y > 0) 
+            m_TimeMultiplier += m_TimeMultIncrement;
+        else if (inputValue.y < 0)
+            m_TimeMultiplier -= m_TimeMultIncrement;
+
+        if(PrintDebugLogs) Debug.Log($"Current time multiplier is: {m_TimeMultiplier}.");
+        Time.timeScale = 1.0f * m_TimeMultiplier;
+    }
+
     public void StartIntro()
     {
         _menuController.HideCurrentMenu();
@@ -27,6 +79,7 @@ public class CutScene : MonoBehaviour
         if (PrintDebugLogs) Debug.Log($"--- CutScene.StartIntro() SETTING SORTING ORDER TO -* 10 *- IT IS ACTUALLY ({_cutsceneCanvas.sortingOrder}) ---");
         _animator.SetTrigger(IntroTriggerTag);
         Time.timeScale = 1.0f;
+        m_CutSceneStarted = true;
         if (PrintDebugLogs) Debug.Log($"--- CutScene.StartIntro() ({_animator}) setTrigger by string ({IntroTriggerTag}) ---");
     }
     private void FinishedIntro()
@@ -34,6 +87,11 @@ public class CutScene : MonoBehaviour
         if (PrintDebugLogs) Debug.Log($"--- CutScene.FinishedIntro() Called) ---");
         _cutsceneCanvas.sortingOrder = 0;
         _cutsceneCanvas.enabled = false;
+
+        m_CutSceneStarted = false;
+        m_TimeMultiplier = 1.0f;
+        Time.timeScale = 1.0f;
+
         _menuController.StartGameLevelOne();
     }
     public void StartOutro()
@@ -42,6 +100,7 @@ public class CutScene : MonoBehaviour
         _cutsceneCanvas.sortingOrder = 10;
         Time.timeScale = 1.0f;
         _animator.SetTrigger(OutroTriggerTag);
+        m_CutSceneStarted = true;
         if (PrintDebugLogs) Debug.Log($"--- CutScene.StartOutro() ({_animator}) setTrigger by string ({OutroTriggerTag}) ---");
         // open credits? go to title?
     }
@@ -50,6 +109,11 @@ public class CutScene : MonoBehaviour
         if (PrintDebugLogs) Debug.Log($"--- CutScene.FinishedOutro() Called) ---");
         _cutsceneCanvas.sortingOrder = 0;
         _cutsceneCanvas.enabled = false;
+
+        m_CutSceneStarted = false;
+        m_TimeMultiplier = 1.0f;
+        Time.timeScale = 1.0f;
+
         GameManager.Instance.player.GetComponent<PlayerController>().Initialize();
         GameManager.Instance.player.GetComponent<PlayerInventory>().Initialize();
         GameManager.Instance.player.GetComponent<PlayerHealth>().Initialize();
